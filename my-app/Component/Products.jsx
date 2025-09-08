832a2a67de3d90c0b28263ff4d24cf5022085f30
Shahab Dad <shahabdad@gmail.com>
2025-09-08 15:22:19 +0500

    create product

--- my-app/Component/Products.jsx
+++ my-app/Component/Products.jsx
@@ -17,7 +17,25 @@ import ProductCard from "./ProductCard";
         if(sortType === 'mostExpersive') return b.price  - a.price;   
             return 0; 
      
-     } );
+     } );    
+     return(
+        <div  >
+            <div className="text-center mt-[50px]"> 
+                <h2 className="font-['Lato'] text-zinc-800 text-4xl font-black mb-5">
+                All Products
+                </h2>
+                <select
+                value={sortType}
+                onChange={(e) => setSortType(e.target.value)}
+                className="border border-cyan-800 rounded-xl font-['Lato'] text-zinc-800 font-bold  shadow-xl bg-zinc-300 outline-none "
+                >
+                    <option>Select  sort type  </option>
+                    <option value="cheapest">Cheapest</option>
+                    <option value="mostExpersive">Most Expensive</option>
+                </select>
 
-    
+                </div>
+                <div  className="w-full grid  grid-cols-1 sm:grid-cols-2 m"
+            </div>
+     )
  }
