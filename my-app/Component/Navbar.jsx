4b6cd39080f078d9b2d85bad3c05c2df0cb2a3c3
Shahab Dad <shahabdad@gmail.com>
2025-09-12 17:21:08 +0500

    addcart icon work

--- my-app/Component/Navbar.jsx
+++ my-app/Component/Navbar.jsx
@@ -1,5 +1,5 @@
 import { FaShoppingBag } from "react-icons/fa";
-export default function Navbar() {
+export default function Navbar({cartCount}) {
   return (
     <nav className="w-full flex justify-around items-center text-gray-700 text-lg font-bold py-2 bg-[#ffffff29] fixed top-0 left-0 z-10 backdrop-blur-3xl">
       {/* left side */}
@@ -86,9 +86,15 @@ export default function Navbar() {
             >
               <path d="M1015.66 284a31.82 31.82 0 0 0-25.998-13.502H310.526l-51.408-177.28c-20.16-69.808-68.065-77.344-87.713-77.344H34.333c-17.569 0-31.777 14.224-31.777 31.776S16.78 79.425 34.332 79.425h137.056c4.336 0 17.568 0 26.593 31.184l176.848 649.936c3.84 13.712 16.336 23.183 30.591 23.183h431.968c13.409 0 25.376-8.4 29.905-21.024l152.256-449.68c3.504-9.744 2.048-20.592-3.888-29.024zM815.026 720.194H429.539L328.387 334.066h616.096zM752.003 848.13c-44.192 0-80 35.808-80 80s35.808 80 80 80 80-35.808 80-80-35.808-80-80-80zm-288 0c-44.192 0-80 35.808-80 80s35.808 80 80 80 80-35.808 80-80-35.808-80-80-80z"></path>
             </svg>
-            <span className="w-5 h-5 flex font-semibold items-center justify-center text-white text-xs -top-3 -right-3 bg-rose-500 absolute rounded-full">
+            {/* <span className="w-5 h-5 flex font-semibold items-center justify-center text-white text-xs -top-3 -right-3 bg-rose-500 absolute rounded-full">
               1
-            </span>
+              
+            </span> */}
+          {cartCount > 0 && ( 
+            <span className="w-5 h-5 flex font-semibold items-center justify-center text-white text-xs -top-2  -right-2   bg-rose-500  absolute  "
+          )
+
+          }
           </a>
         </li>
       </ul>
--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,15 +5,17 @@ import Hero from '../Component/Hero'
 import Products from '../Component/Products'
 
 function App() {
-
-
+const [cartCount, setCartCount]  = useState(0); 
+const  handleAddToCart   = () =>  { 
+  setCartCount((prev) => prev + 1); 
+}
   return (
     <> 
-      <Navbar />
+      <Navbar    cartCount={cartCount }/>
       <div className=""> {/* padding so content not hidden */}
-        <Hero />
+        <Hero     />
         
-      <Products />
+      <Products  />
 
       </div>
     </>
